6207eecd7639d9e00cc88876ac79d480612637c1
Kush <kushagrasaxena0913@gamil.com>
2026-01-31 23:40:30 +0530

    datatype

---
+++ .github/appmod/appcat/datatype.js
@@ -0,0 +1,24 @@
+console.log("Hitesh")
+
+let name="Kush";
+let age=18;
+let isLog=false;
+let state;
+
+// console.log(typeof name); //string
+// console.log(typeof age); //number
+// console.log(typeof isLog); //boolean
+// console.log(typeof state); //undefined
+
+// console.log(typeof undefined); //undefined
+// console.log(typeof null); //object
+
+let arr = [1,2,34];
+console.log(typeof arr);
+
+let obj = {
+    name : "JS",
+    year: "1995"
+};
+
+console.log(typeof obj)

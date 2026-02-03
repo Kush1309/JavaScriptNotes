dc517be09bc5a59f0b37c3f945c9f94d2b73bc4a
Kush <kushagrasaxena0913@gamil.com>
2026-02-03 12:15:22 +0530

    message

---
+++ map.js
@@ -0,0 +1,25 @@
+// MAp : Map is a key or value like structure in this method only unique values are present 
+
+const map = new Map();
+map.set('USA', "United state of America");
+map.set("IN","India");
+// console.log(map);
+// for (const key of map) {
+//     console.log(key)
+// }
+
+// for(const [key,value] of map){
+//     console.log(key + ":-"+ value);
+// }
+
+//its is similar like array 
+
+//lekin for of iterater object me nahi chalta hai
+
+const ObjMap=[
+    name="Kushagra",
+    college="LPU" 
+]
+for (const i of ObjMap) {
+    console.log(i);
+}
